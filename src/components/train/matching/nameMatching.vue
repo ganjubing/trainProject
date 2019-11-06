77f236f3475e4c58ee981732bb161b1eea0ef0c3
Mr_Gan2213 <beyond4916293@163.com>
2019-11-06 18:25:44 +0800

    modify

--- src/components/train/matching/nameMatching.vue
+++ src/components/train/matching/nameMatching.vue
@@ -2,16 +2,16 @@
   <div class="matching">
     <span class="surplusTime">{{surplusTime}}</span>
     <div v-if="ready">
-      <img src="../../../assets/matching/personInfo.png" style="width: 100%;height: 100%;" />
+      <img src="@/assets/matching/personInfo.png" style="width: 100%;height: 100%;" />
       <div style="width: 20.5%;height: 20%;position: absolute;top:39.5%;left: 41.5%;">
         <div style="width: 50%;float: left;height: 100%;position: relative;">
-          <img src="../../../assets/matching/images/wuyanzu.jpg" style="width: 60%;height: 60%;position: absolute;top:50%;left: 50%;margin-left: -30%;margin-top: -30%;border-radius:10px;" />
+          <img src="@/assets/matching/images/wuyanzu.jpg" style="width: 60%;height: 60%;position: absolute;top:50%;left: 50%;margin-left: -30%;margin-top: -30%;border-radius:10px;" />
         </div>
         <div style="width: 50%;float: left;height: 100%;">
           <span style="font-size: 30px;color: white;text-align:center;display:block;position: relative;top:50%;transform:translateY(-50%);">吴彦祖</span>
         </div>
       </div>
-      <img src='../../../assets/btnContinu.png' style="position: absolute;left: 45%;bottom: 20%;" />
+      <img src='@/assets/btnContinu.png' style="position: absolute;left: 45%;bottom: 20%;" />
     </div>
     <div style="border: 1px solid #00FF00; width: 73%;height:68.5%;position: absolute;left: 14.4%;top:14%;">
       <div class="divItem" v-for="item in 32" style="">
@@ -22,12 +22,34 @@
 </template>
 
 <script>
+  import {userList} from '@/data/matching_users.js'
+  import {
+    randonArr,
+    randomLimit,
+    getRandomArr,
+    randomNumBoth,
+  } from '@/utils/common.js'
   export default {
     data() {
       return {
         surplusTime: 299,
         ready: false,
+        users:[],
+        currentIndex:0,
+        currentPerson:[],
       }
+    },
+    mounted() {
+      this.users=userList();
+      console.log(this.users)
+    },
+    methods:{
+      getCurrentPerson(isFirst){
+        this.currentPerson.push(this.users[this.currentIndex]);
+        if(isFirst){
+          this.currentPerson.push(this.users[this.currentIndex+1]);
+        }
+      },
     },
   }
 </script>
@@ -61,6 +83,6 @@
     width: 9%;
     background-image: url(../../../assets/matching/person.png);
     background-repeat: no-repeat;
-    background-size:cover;
+    background-size:100% 100%;
   }
 </style>
---
+++ src/data/matching_users.js
@@ -0,0 +1,109 @@
+export const userList = () => {
+  var list = [{
+      nameEN: 'chenguanxi',
+      nameCN: '陈冠希',
+      img: require('@/assets/matching/images/chenguanxi.jpg')
+    },
+    {
+      nameEN: 'dengchao',
+      nameCN: '邓超',
+      img: require('@/assets/matching/images/dengchao.jpg')
+    },
+    {
+      nameEN: 'guofucheng ',
+      nameCN: '郭富城',
+      img: require('@/assets/matching/images/guofucheng.jpg')
+    },
+    {
+      nameEN: 'gutianle',
+      nameCN: '古天乐',
+      img: require('@/assets/matching/images/gutianle.jpg')
+    },
+    {
+      nameEN: 'huge',
+      nameCN: '胡歌',
+      img: require('@/assets/matching/images/huge.jpg')
+    },
+    {
+      nameEN: 'yaoming',
+      nameCN: '姚明',
+      img: require('@/assets/matching/images/yaoming.jpg')
+    },
+    {
+      nameEN: 'jinchengwu',
+      nameCN: '金城武',
+      img: require('@/assets/matching/images/jinchengwu.jpg')
+    },
+    {
+      nameEN: 'liming',
+      nameCN: '黎明',
+      img: require('@/assets/matching/images/liming.jpg')
+    },
+    {
+      nameEN: 'liudehua',
+      nameCN: '刘德华',
+      img: require('@/assets/matching/images/liudehua.jpg')
+    },
+    {
+      nameEN: 'liushishi',
+      nameCN: '刘诗诗',
+      img: require('@/assets/matching/images/liushishi.jpg')
+    },
+    {
+      nameEN: 'liuyifei',
+      nameCN: '刘亦菲',
+      img: require('@/assets/matching/images/liuyifei.jpg')
+    },
+    {
+      nameEN: 'mayun',
+      nameCN: '马云',
+      img: require('@/assets/matching/images/mayun.jpg')
+    },
+    {
+      nameEN: 'tongyiya',
+      nameCN: '佟丽娅',
+      img: require('@/assets/matching/images/tongliya.jpg')
+    },
+    {
+      nameEN: 'wanglihong',
+      nameCN: '王力宏',
+      img: require('@/assets/matching/images/wanglihong.jpg')
+    },
+    {
+      nameEN: 'wuyanzu',
+      nameCN: '吴彦祖',
+      img: require('@/assets/matching/images/wuyanzu.jpg')
+    },
+    {
+      nameEN: 'yangmi',
+      nameCN: '杨幂',
+      img: require('@/assets/matching/images/yangmi.jpg')
+    },
+    {
+      nameEN: 'yijianlian',
+      nameCN: '易建联',
+      img: require('@/assets/matching/images/yijianlian.jpg')
+    },
+    {
+      nameEN: 'zhangguorong',
+      nameCN: '张国荣',
+      img: require('@/assets/matching/images/zhangguorong.jpg')
+    },
+    {
+      nameEN: 'zhangxueyou',
+      nameCN: '张学友',
+      img: require('@/assets/matching/images/zhangxueyou.jpg')
+    },
+    {
+      nameEN: 'zhaoliying',
+      nameCN: '赵丽颖',
+      img: require('@/assets/matching/images/zhaoliying.jpg')
+    },
+    {
+      nameEN: 'zhoujielun',
+      nameCN: '周杰伦',
+      img: require('@/assets/matching/images/zhoujielun.jpg')
+    },
+  ];
+  return list;
+}
