307ef1e043d7df11ee92dab2190ee866facd81c4
Mr_Gan2213 <beyond4916293@163.com>
2019-11-22 18:53:42 +0800

    modify

--- src/components/train/visualSpace/visualSpace.vue
+++ src/components/train/visualSpace/visualSpace.vue
@@ -1,32 +1,34 @@
 <template>
   <div class="visualSpace">
     <span class="surplusTime">{{surplusTime}}</span>
-    <div :class="{'incorrectbg':isCorrect===false,'correctbg':isCorrect===true}" style="position: absolute;left: 33.4%;top:25%;width:36.5%;height:57%;">
-      <div style="width: 50%;height:100%;">
-        <div :class="{'moveLeft':ismove}"style="position: absolute;top:25%;left:20%;">
-          <img  :src="currentWord.img" style="width: 41%;height: 100%;" />
-        </div>
+    <div :class="{'incorrectbg':isCorrect===false,'correctbg':isCorrect===true}" style="position: absolute;left: 33.2%;top:25%;width:36.5%;height:57%;">
+      <div style="">
 
+        <transition name="no-mode-fade" v-on:after-enter="afterEnter">
+          <div v-if="show" key="current" style="position: absolute;top:20%;left:30%;width: 30%;height:40%;">
+            <img :src="currentWord.img"  style="width: 100%;height: 100%;" />
+          </div>
+          <div v-else key="prev" style="position: absolute;top:20%;left:30%;width: 30%;height:40%;">
+            <img :src="prevWord.img"  style="width: 100%;height: 100%;" />
+          </div>
+        </transition>
       </div>
-      <!-- <div style="width: 50%;height: 100%;float: left;">
-        <img :src="fontColor.img" style="width: 41%;height: 100%;" />
-      </div> -->
-      <!-- <img v-if="isCorrect===true" src="../../../assets/correct.png" style="position:absolute;left: 43%;top:11%;z-index: 9999 ;" />
-      <img v-if="isCorrect===false" src="../../../assets/incorrect.png" style="position:absolute;left: 42%;top:11%;z-index: 9999 ;" /> -->
     </div>
     <div v-if="isStart===false" style="position: absolute;left: 28%;bottom:5%;width:40%;height:20%;">
       <div><img src="@/assets/visualSpace/tip.png" style="width: 60%;height: 60%;" /></div>
       <div style="margin-top: 4%;width: 100%;height: 50%;">
         <img @click="nexStep" src="@/assets/visualSpace/start.png" style="width: 30%;height:90%;" />
-        </div>
+      </div>
     </div>
     <div v-else style="position: absolute;left: 20%;bottom:5%;width:60%;height:20%;">
       <div><img src="@/assets/visualSpace/currentTip.png" style="width: 50%;height: 50%;" /></div>
       <div style="width:50%;height: 80%;float: left;">
-        <img :src="equalimg" style="width: 52.5%;height: 100%;" @mouseover="changeOver('equal')" @mouseout="changeOut('equal')" @click="checkItem(true)" />
+        <img :src="equalimg" style="width: 52.5%;height: 100%;" @mouseover="changeOver('equal')" @mouseout="changeOut('equal')"
+          @click="checkItem(true)" />
       </div>
       <div style="width: 50%;height: 80%;float: left;">
-        <img :src="unequalimg" style="width: 52.5%;height: 100%;" @mouseover="changeOver('unequal')" @mouseout="changeOut('unequal')" @click="checkItem(false)" />
+        <img :src="unequalimg" style="width: 52.5%;height: 100%;" @mouseover="changeOver('unequal')" @mouseout="changeOut('unequal')"
+          @click="checkItem(false)" />
       </div>
     </div>
     <div class="divResult" v-show="isFinish">
@@ -35,7 +37,7 @@
       <span style="position: absolute;left: 53%;top: 51%;color:#357dd3;font-size:30px;">{{correctNumber}}</span>
       <span style="position: absolute;left: 47%;top: 60%;color:#d7112f;font-size:50px;font-weight:700;">{{Math.round(correctNumber*10.5)}}</span>
       <img style="position: absolute;left: 44%;bottom:3.5%;width: 12.5%;height: 10%;" src="@/assets/btnContinu.png"
-       @click="continu" />
+        @click="continu" />
     </div>
   </div>
 </template>
@@ -54,44 +56,58 @@
   export default {
     data() {
       return {
+        show: true,
         totalDuration: 120, //计时器总时长(秒)
         surplusTime: 120, //当前剩余时长
         isTimeout: false, //是否时间已到
         isFinish: false, //是否答题结束
-        isStart:false,
-        ismove:false,
-        nextQuestionTime:500, //隔多久进入下一题
+        isStart: false,
+        ismove: false,
+        nextQuestionTime: 500, //隔多久进入下一题
         intervalTime: null,
         equalimg: equalImg,
         unequalimg: unequalImg,
-        itemArray:[{name:'0',img:require("@/assets/visualSpace/0.png")},
-        {name:'1',img:require("@/assets/visualSpace/1.png")},
-        {name:'2',img:require("@/assets/visualSpace/2.png")},
-        {name:'3',img:require("@/assets/visualSpace/3.png")},],
-        currentWord:{},//本题随机的字体名称对象
-        prevWord:{},//本题随机的字体颜色对象
-        isCorrect:null,
+        itemArray: [{
+            name: '0',
+            img: require("@/assets/visualSpace/0.png")
+          },
+          {
+            name: '1',
+            img: require("@/assets/visualSpace/1.png")
+          },
+          {
+            name: '2',
+            img: require("@/assets/visualSpace/2.png")
+          },
+          {
+            name: '3',
+            img: require("@/assets/visualSpace/3.png")
+          },
+        ],
+        currentWord: {}, //本题随机的字体名称对象
+        prevWord: {}, //本题随机的字体颜色对象
+        isCorrect: null,
         totalAnswerNumber: 0, //总答题数
         correctNumber: 0, //答对数量
-        totalUseTime:0,//毫秒
+        totalUseTime: 0, //毫秒
         beginTime: null, //本题的开始时间
         endTime: null, //本题结束时间
       }
     },
-    computed:{
+    computed: {
       accuracy: function() {
         return (this.correctNumber / this.totalAnswerNumber).toFixed(2) * 100;
       }
     },
-    mounted(){
+    mounted() {
       this.continu();
     },
     methods: {
       intervalTimer() {
         this.totalAnswerNumber0;
-        this.correctNumber=0;
-        this.totalUseTime=0;
-        this.isTimeout=false;
+        this.correctNumber = 0;
+        this.totalUseTime = 0;
+        this.isTimeout = false;
         // 计时器为空，操作
         if (this.intervalTime != null) return;
         this.intervalTime = setInterval(() => {
@@ -114,45 +130,49 @@
         else
           this.unequalimg = unequalImg;
       },
-      initData(){
-        this.isCorrect=null;
-        this.prevWord=this.currentWord!=null?this.currentWord:this.prevWord;
-        this.currentWord=getRandomArr(this.itemArray, 1)[0];
+      initData() {
+        this.isCorrect = null;
+        this.prevWord = this.currentWord != null ? this.currentWord : this.prevWord;
+        this.currentWord = getRandomArr(this.itemArray, 1)[0];
         this.beginTime = new Date(); //答题开始计时
       },
-      checkItem(val){
-        if(this.isCorrect!=null)return;
-        this.totalAnswerNumber+=1;
+      checkItem(val) {
+
+        if (this.isCorrect != null) return;
+        this.totalAnswerNumber += 1;
         this.endTime = new Date();
-        var isEqual=this.currentWord.name===this.prevWord.name;
-        this.playAudio(isEqual===val)
+        var isEqual = this.currentWord.name === this.prevWord.name;
+        this.playAudio(isEqual === val)
         var duration = this.endTime.getTime() - this.beginTime.getTime();
-        this.totalUseTime+=duration;
-         if(isEqual==val){
-           this.correctNumber+=1;
-           this.isCorrect=true;
-         }else{
-           this.isCorrect=false;
-         }
-         setTimeout(()=>{
-           this.nexStep();
-         },this.nextQuestionTime);
+        this.totalUseTime += duration;
+        if (isEqual == val) {
+          this.correctNumber += 1;
+          this.isCorrect = true;
+        } else {
+          this.isCorrect = false;
+        }
+        setTimeout(() => {
+          this.nexStep();
+        }, this.nextQuestionTime);
       },
-      nexStep(){
-        this.isStart=true;
-        this.ismove=true;
-        if(this.isTimeout){
-          this.isFinish=true;
-        }else{
+      nexStep() {
+        this.isStart = true;
+        this.show=!this.show;
+        if (this.isTimeout) {
+          this.isFinish = true;
+        } else {
           this.initData();
         }
       },
-      continu(){
-        this.isStart=false;
-        this.surplusTime=this.totalDuration;
-        this.isFinish=false;
+      continu() {
+        this.isStart = false;
+        this.surplusTime = this.totalDuration;
+        this.isFinish = false;
         this.intervalTimer();
         this.initData();
+      },
+      afterEnter() {
+        // this.show=true;
       },
     },
   }
@@ -169,14 +189,16 @@
     background-position: center;
     -moz-background-size: 100% 100%;
   }
+
   .visualSpace .surplusTime {
     position: absolute;
-    right:10%;
-    top:0%;
-    color:#f50c38;
+    right: 10%;
+    top: 0%;
+    color: #f50c38;
     font-weight: bolder;
     font-size: 30px;
   }
+
   .divResult {
     width: 100%;
     height: 100%;
@@ -189,7 +211,8 @@
     background-position: center, center;
     z-index: 9999;
   }
-  .correctbg{
+
+  .correctbg {
     width: 100%;
     height: 100%;
     position: absolute;
@@ -198,10 +221,11 @@
     background-image: url(../../../assets/visualSpace/correctbg.png);
     background-repeat: no-repeat;
     background-size: 100% 100%;
-   /* background-position: center, center; */
+    /* background-position: center, center; */
     /* z-index: 9999; */
   }
-  .incorrectbg{
+
+  .incorrectbg {
     width: 100%;
     height: 100%;
     position: absolute;
@@ -210,31 +234,24 @@
     background-image: url(../../../assets/visualSpace/inCorrectbg.png);
     background-repeat: no-repeat;
     background-size: 100% 100%;
-   /* background-position: center, center; */
+    /* background-position: center, center; */
     /* z-index: 9999; */
   }
-  .moveLeft {
-    animation: leftAD 0.5s infinite;
-    -webkit-animation: leftAD 0.5s infinite;
-    animation-iteration-count: 1;
-  }
-  @keyframes leftAD {
-    from {
-      left: 10%;
-    }
-
-    to {
-      left: -20%;
-    }
+  .no-mode-fade-enter-active,
+  .no-mode-fade-leave-active {
+    transition: all 0.5s
   }
 
-  @-webkit-keyframes leftAD {
-    from {
-      left: 10%;
-    }
+  .no-mode-fade-enter,
+  .no-mode-fade-leave-active {
+    opacity: 0;
+  }
 
-    to {
-      left: -20%;
-    }
+  .no-mode-fade-enter {
+    transform: translateX(70px);
+  }
+
+  .no-mode-fade-leave-active {
+    transform: translateX(-70px);
   }
 </style>
